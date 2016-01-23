127910e2d37dafccb9d5040c3b30b30d78bd8999
Dale Myszewski II <dale.myszewski@gmail.com>
2016-01-22 18:17:26 -0600

    Build script

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,4 +1,6 @@
 class Upload < ActiveRecord::Base
+  #http://railscasts.com/episodes/134-paperclip
+  #http://josephndungu.com/tutorials/ajax-file-upload-with-dropezonejs-and-paperclip-rails
   has_attached_file :image, :styles => { :thumb => "100x100>" },
                     :url  => "/assets/pictures/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/pictures/:id/:style/:basename.:extension"
---
+++ build.sh
@@ -0,0 +1,6 @@
+#
+#
+docker build -t daleslab/site .
+docker stop daleslab/livesite
+docker rm daleslab/livesite
+docker run -d -p 8080:80 --name=daleslab/livesite -e DALESLAB_SECRET_KEY_BASE=secretkey -v /root/file-uploads/daleslab:/app/public/assets daleslab/site
