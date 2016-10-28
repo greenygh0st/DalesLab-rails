8dd784e958eaaf6ecd2790be8b68b152d5b49f55
Dale Myszewski II <dale.myszewski@gmail.com>
2016-10-28 01:55:11 -0500

    Added env to paperclip s3

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,8 +2,8 @@ class Upload < ActiveRecord::Base
   #http://railscasts.com/episodes/134-paperclip
   #http://josephndungu.com/tutorials/ajax-file-upload-with-dropezonejs-and-paperclip-rails
   has_attached_file :image, :styles => { :thumb => "100x100>" },
-                    :url  => "/assets/pictures/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/assets/pictures/:id/:style/:basename.:extension"
+                    #:url  => "/assets/pictures/:id/:style/:basename.:extension",
+                    :path => "/daleslab/#{Rails.env}/uploads/:id/:style/:basename.:extension"
 
 	validates_attachment 	:image,
 				:presence => true,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,18 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.paperclip_defaults = {
+    storage: :s3,
+    :s3_protocol => :https,
+    s3_region: ENV.fetch('AWS_REGION'),
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
